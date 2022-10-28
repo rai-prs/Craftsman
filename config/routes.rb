7866f35a036960eed02bf79a846731afffa19a2a
白井拓実 <>
2022-10-28 17:15:13 +0900

    gem deviceの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to:    'users#index'
-  get         'users/index'
+  devise_for :users
+  root to:    'home#index'
+  get         'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
